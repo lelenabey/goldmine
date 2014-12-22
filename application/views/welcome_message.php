c7825b09a2617033c47b3b17dc5655d5d82fc84e
lelenabey <lelenabey@gmail.com>
2014-12-22 13:59:59 -0500

    all dem files

---
+++ application/views/welcome_message.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/styles.css" rel="stylesheet">
+		<script type="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+		<script type="js/bootstrap.js"></script>
+	</head>
+	<body>
+		<h1>Surprise motherfucker</h1>
+		<div class="embed-responsive embed-responsive-16by9">
+  			<iframe class="embed-responsive-item" src="//www.youtube.com/embed/Nj8r3qmOoZ8"></iframe>
+		</div>
+	</body>
+</html>
