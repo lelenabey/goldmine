1b5ffb115f46729d006c294856ddf29be3c8d842
lelenabey <lelenabey@gmail.com>
2014-12-23 12:49:06 -0500

    added list and a container for video

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -9,8 +9,22 @@
 	</head>
 	<body>
 		<h1>Surprise motherfucker</h1>
-		<div class="embed-responsive embed-responsive-16by9">
-  			<iframe class="embed-responsive-item" src="//www.youtube.com/embed/Nj8r3qmOoZ8"></iframe>
+		<div class="main">
+			<div class="panel panel-default">
+				  <div class="panel-body">
+				    <div class="embed-responsive embed-responsive-16by9">
+	  					<iframe class="embed-responsive-item" src="//www.youtube.com/embed/Nj8r3qmOoZ8"></iframe>
+					</div>
+				  </div>
+				  <div class="panel-footer">Panel footer</div>
+			</div>
 		</div>
+		<ul class="list-group">
+  			<li class="list-group-item">
+			    <span class="badge">14</span>
+			    Cras justo odio
+		  	</li>
+		</ul>
+		
 	</body>
 </html>
